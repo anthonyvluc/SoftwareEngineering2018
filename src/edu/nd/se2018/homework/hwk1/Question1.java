7829e9a2ba215b48e5df6c1de3e2495e40566b99
Anthony Luc <anthonyvanluc@gmail.com>
2018-08-31 20:50:21 -0400

    finish question 1

--- src/edu/nd/se2018/homework/hwk1/Question1.java
+++ src/edu/nd/se2018/homework/hwk1/Question1.java
@@ -1,10 +1,26 @@
 package edu.nd.se2018.homework.hwk1;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Question1 {
 		
 	public Question1(){}
 	
 	public int getSumWithoutDuplicates(int[] numbers){
-		return 0;	
+		int sum = 0;
+		Set<Integer> numSet = new HashSet<Integer>();
+
+		// Add numbers to a set to remove duplicates.
+		for (int number : numbers) {
+			numSet.add(number);
+		}
+		
+		// Sum the unique numbers.
+		for (int number : numSet) {
+			sum += number;
+		}
+		
+		return sum;
 	}
 }
