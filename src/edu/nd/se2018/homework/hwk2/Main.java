df8e30ef0ba2ac817567ffd0dd19893b076ab7bf
Anthony Luc <anthonyvanluc@gmail.com>
2018-09-10 01:41:43 -0400

    Buff Ahri

--- src/edu/nd/se2018/homework/hwk2/Main.java
+++ src/edu/nd/se2018/homework/hwk2/Main.java
@@ -11,7 +11,7 @@ public class Main {
 		Horse pharoah = new Horse(0.0, "Pharoah", 201, 1.0, new SteadyRunStrategy());
 		Horse otto = new Horse(0.0, "Otto", 301, 0.7, new SlowStartStrategy());
 		Horse princess = new Horse(0.0, "Princess", 401, 0.6, new EarlySprintStrategy());
-		Horse ahri = new Horse(0.0, "Ahri", 501, 0.4, new SlowStartStrategy());
+		Horse ahri = new Horse(0.0, "Ahri", 501, 0.9, new SlowStartStrategy());
 		
 		// Enroll horses to race.
 		race.enrollHorse(blitz);
