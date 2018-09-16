80ba7580da716d4fb7aefa5143c1261869bdac6f
Anthony Luc <anthonyvanluc@gmail.com>
2018-09-16 02:24:21 -0400

    added islands

--- src/edu/nd/se2018/homework/hwk3/ColumbusGame/OceanMap.java
+++ src/edu/nd/se2018/homework/hwk3/ColumbusGame/OceanMap.java
@@ -1,35 +1,81 @@
 package edu.nd.se2018.homework.hwk3.ColumbusGame;
 
+import java.awt.Point;
+import java.util.Random;
+
 import javafx.collections.ObservableList;
 import javafx.scene.Node;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
 public class OceanMap {
-	private int dimensions;
-	boolean[][] oceanGrid;
-	private int scale;
 
+	public enum Tile {
+	    OCEAN, ISLAND, PIRATE
+	}
+
+	private int dimensions;
+	Tile[][] oceanGrid;
+	private int scale;
+	
+	private int numIslands 	= 10;
+	private int numPirates 	= 10;
 
 	public OceanMap(int dimension, int scale) {
 		this.dimensions = dimension;
-		this.oceanGrid = new boolean[dimensions][dimensions];
-		this.scale = scale;
+		this.oceanGrid 	= new Tile[dimensions][dimensions];
+		this.scale 		= scale;
+		
+		generateOcean();
+		generateIslands();
 	}
 	
 	public void drawMap(ObservableList<Node> root) {
 		for(int x = 0; x < dimensions; ++x) {
 			for (int y = 0; y < dimensions; ++y) {
 				Rectangle rect = new Rectangle(x*scale, y*scale, scale, scale);
-				rect.setStroke(Color.BLACK);		// Black outline
-				rect.setFill(Color.PALETURQUOISE);	// Ocean color
+				rect.setStroke(Color.BLACK);
+				switch(oceanGrid[x][y]) {
+					case OCEAN:
+						rect.setFill(Color.PALETURQUOISE);
+						break;
+					case ISLAND:
+						rect.setFill(Color.GREEN);
+						break;
+					case PIRATE:
+						break;
+					default:
+						break;
+				}
 				root.add(rect);
-				oceanGrid[x][y] = false;
+			}
+		}
+	}
+
+	public boolean isValidHeroPosition(Point position) {
+		return	((position.x >= 0) && (position.x < dimensions) &&
+				 (position.y >= 0) && (position.y < dimensions) &&
+			     (oceanGrid[position.x][position.y] == Tile.OCEAN));
+	}
+	
+	private void generateOcean() {
+		for(int x = 0; x < dimensions; ++x) {
+			for (int y = 0; y < dimensions; ++y) {
+				oceanGrid[x][y] = Tile.OCEAN;
 			}
 		}
 	}
 	
-	public int getDimensions() {
-		return this.dimensions;
+	private void generateIslands() {
+		int i = 0;
+		while (i < numIslands) {
+			Random rand = new Random();
+			int x = rand.nextInt(dimensions);
+			int y = rand.nextInt(dimensions);
+			if (oceanGrid[x][y] == Tile.OCEAN) {
+				oceanGrid[x][y] = Tile.ISLAND;
+				++i;
+			}
+		}
 	}
 }
--- src/edu/nd/se2018/homework/hwk3/ColumbusGame/Ship.java
+++ src/edu/nd/se2018/homework/hwk3/ColumbusGame/Ship.java
@@ -23,32 +23,28 @@ public class Ship {
 	
 	public void goNorth() {
 		Point newPosition = new Point(this.position.x, this.position.y - 1);
-		if ((newPosition.y >= 0) &&
-			(oceanMap.oceanGrid[newPosition.x][newPosition.y] == false)) {
+		if (oceanMap.isValidHeroPosition(newPosition)) {
 			this.position.setLocation(newPosition);
 		}
 	}
 
 	public void goSouth() {
 		Point newPosition = new Point(this.position.x, this.position.y + 1);
-		if ((newPosition.y < oceanMap.getDimensions()) &&
-			(oceanMap.oceanGrid[newPosition.x][newPosition.y] == false)) {
+		if (oceanMap.isValidHeroPosition(newPosition)) {
 			this.position.setLocation(newPosition);
 		}
 	}
 
 	public void goEast() {
 		Point newPosition = new Point(this.position.x + 1, this.position.y);
-		if ((newPosition.x < oceanMap.getDimensions()) && 
-			(oceanMap.oceanGrid[newPosition.x][newPosition.y] == false)) {
+		if (oceanMap.isValidHeroPosition(newPosition)) {
 			this.position.setLocation(newPosition);
 		}
 	}
 
 	public void goWest() {
 		Point newPosition = new Point(this.position.x - 1, this.position.y);
-		if ((newPosition.x >= 0) &&
-			(oceanMap.oceanGrid[newPosition.x][newPosition.y] == false)) {
+		if (oceanMap.isValidHeroPosition(newPosition)) {
 			this.position.setLocation(newPosition);
 		}
 	}
