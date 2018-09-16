1106b31d645a324a0d5cb9148ce35376330f70d7
Anthony Luc <anthonyvanluc@gmail.com>
2018-09-15 22:36:19 -0400

    init colubmbus game

---
+++ src/edu/nd/se2018/homework/hwk3/ColumbusGame/OceanExplorer.java
@@ -0,0 +1,19 @@
+package edu.nd.se2018.homework.hwk3.ColumbusGame;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class OceanExplorer extends Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
